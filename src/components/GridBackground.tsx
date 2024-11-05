e6811fed8c40e3ee34a578cb187fd040d3e56a3d
XavierElon <xavierelon93@gmail.com>
2024-11-05 14:47:54 -0800

    clean up gridBackground

--- src/components/GridBackground.tsx
+++ src/components/GridBackground.tsx
@@ -53,26 +53,19 @@ const GridBackground = () => {
   return (
     <div className="relative w-full min-h-screen">
       {' '}
-      {/* GridBackground Layers */}
       {/* Base grid background */}
       <div className="absolute inset-0 bg-[#090c10] bg-[length:100px_100px] bg-grid-white/[0.10] z-0"></div>
       {/* Color streaks overlay */}
       <div className="absolute inset-0 bg-gradient-to-br from-purple-900/40 via-blue-900/40 to-teal-900/40 z-10"></div>
       {/* Radial gradient for the faded look */}
       <div className="absolute inset-0 bg-[#090c10] [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] z-10"></div>
-      {/* Beam Animation */}
-      <BackgroundBeams />
       {/* AuroraBackground Overlay */}
       {/* <AuroraBackground className="absolute inset-0 z-20" showRadialGradient={true} /> */}
       {/* Main Content */}
       <div className="">
-        {/* Navbar */}
-        <FloatingNavbar navItems={navItems} className="z-40" />
-        {/* <Navbar tab={tab} setTab={setTab} left={x} sliderWidth={w} /> */}
         {/* Content */}
         <div className="relative z-40 flex items-center justify-center">
           <GridLayout tab={tab} setTab={setTab} left={x} sliderWidth={w} />
-          <CursorGradient />
         </div>
       </div>
     </div>
